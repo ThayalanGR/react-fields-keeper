00c1237165ef03f1eafc1a7f43a63cf25432559d
ThayalanGR <grthayalan18@gmail.com>
2023-05-12 20:35:56 +0530

    fix: react version update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
-)
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
